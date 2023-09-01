5e8cef3442d78d2644fb95251f6c830dd929316f
seutil <seutil@inbox.ru>
2023-09-02 00:36:27 +0300

    Add index() view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import Task
+
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'index.html', {
+        'tasks': tasks,
+    })
