b506d57c8c4e6be7fc0274fe90f7fa3065aacd0a
seutil <seutil@inbox.ru>
2023-09-02 00:48:29 +0300

    Remove content field from the model and make migrations

--- core/models.py
+++ core/models.py
@@ -3,5 +3,4 @@ from django.db import models
 
 class Task(models.Model):
     name = models.CharField(max_length=255)
-    content = models.CharField(max_length=255)
     is_closed = models.BooleanField()
