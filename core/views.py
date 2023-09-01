9e0d38593b193a995f720ffd85148a918391197a
seutil <seutil@inbox.ru>
2023-09-02 01:07:24 +0300

    Implement new task creation

--- core/views.py
+++ core/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
 
 from .models import Task
+from .forms import NewTaskForm
 
 
 def index(request):
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = NewTaskForm()
+
     tasks = Task.objects.all()
     return render(request, 'index.html', {
+        'form': form,
         'tasks': tasks,
     })
