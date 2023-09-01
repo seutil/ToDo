7c96ae755743fba32f2a15a6154fe75c1209ba3e
seutil <seutil@inbox.ru>
2023-09-02 01:33:21 +0300

    Add url for open view

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,5 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:task_pk>/close/', views.close, name='close'),
+    path('<int:task_pk>/open/', views.open, name='open'),
 ]
