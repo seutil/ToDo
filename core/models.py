d3f8980f80840c98e9564b40d064afd1773e956e
seutil <seutil@inbox.ru>
2023-09-02 01:05:12 +0300

    Update Task model is_closed default value and make migrations

--- core/models.py
+++ core/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Task(models.Model):
     name = models.CharField(max_length=255)
-    is_closed = models.BooleanField()
+    is_closed = models.BooleanField(default=False)
