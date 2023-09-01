3b0b2f3991e4effcba2f8c90a0707adf8d59df54
seutil <seutil@inbox.ru>
2023-09-02 00:36:11 +0300

    Add CharField max_length kwarg

--- core/models.py
+++ core/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Task(models.Model):
-    name = models.CharField()
-    content = models.CharField()
+    name = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
     is_closed = models.BooleanField()
